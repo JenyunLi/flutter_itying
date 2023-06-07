7d1411b17cf33a844db4b25fc40a4d5e726e0141
Jenyuan Li <lijenyun@gmail.com>
2023-06-07 09:54:27 +0800

    update 32

---
+++ 32-Explicit_Animate/lib/4-SlideTransition.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(
+          vsync: this, 
+          duration: const Duration(seconds: 2),
+          lowerBound: 0.5,
+          upperBound: 1.5,
+        )
+          // ..repeat(reverse: true)
+        ;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.refresh),
+          onPressed: () {
+            _controller.repeat(reverse: true);
+          }),
+        appBar: AppBar(
+          title: const Text("Title"),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            SlideTransition( // Tween 配置运动的參数
+              // position: _controller.drive(Tween(begin:const Offset(0,0), end: const Offset(0.5,0.5))),
+              position: Tween(begin:const Offset(0,0), end: const Offset(0,2))
+                .chain(CurveTween( // 配置动画效果
+                  curve: Curves.bounceInOut
+                ))
+                .chain(CurveTween( // 𢑐前时间点的百分之間20开始运动到80結束
+                  curve: const Interval(0.2,0.8)
+                ))
+                .animate(_controller),
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.red,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const SizedBox(height: 40),
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.forward();
+                    },
+                    child: const Text("forward")),
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.reverse();
+                    },
+                    child: const Text("reverse")),
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.stop();
+                    },
+                    child: const Text("stop")),
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.reset();
+                    },
+                    child: const Text("reset")),
+                    ],
+            )
+          ],
+        )));
+  }
+}
